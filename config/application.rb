8baf1935c638dbda308c9b85df9c2526613fa799
Andre Menezes <djdedemenezes@gmail.com>
2024-08-15 13:52:50 -0300

    add sidekiq as job adapter

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module RailsMainoDedevLab
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Config queue adapter
+    config.active_job.queue_adapter = :sidekiq
   end
 end
