a7b2193fca79d49ac1030483edd66b5c9074cd9a
Andre Menezes <djdedemenezes@gmail.com>
2024-08-15 13:00:11 -0300

    add upload document basic feature

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,20 @@
+class DocumentsController < ApplicationController
+  def new
+    @document = Document.new
+  end
+
+  def create
+    @document = Document.new(document_params)
+    if @document.save
+      redirect_to new_document_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def document_params
+    params.fetch(:document, {}).permit(:xml)
+  end
+end
