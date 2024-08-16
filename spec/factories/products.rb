704e72404ba4fe310921ea89659e4cea818affb5
Andre Menezes <djdedemenezes@gmail.com>
2024-08-16 02:18:31 -0300

    add product model without validations

---
+++ spec/factories/products.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :product do
+    name { "MyString" }
+    ncm { "MyString" }
+    cfop { "MyString" }
+    u_com { "MyString" }
+    q_com { 1.5 }
+    v_un_com { 1 }
+  end
+end
