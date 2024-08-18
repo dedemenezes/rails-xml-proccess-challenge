dc524d7aded077c0f360d5a5e69db34b3cdb728d
Andre Menezes <djdedemenezes@gmail.com>
2024-08-18 02:13:08 -0300

    refac/cleaning up files

--- app/jobs/create_document_taxes_job.rb
+++ app/jobs/create_document_taxes_job.rb
@@ -2,12 +2,9 @@ class CreateDocumentTaxesJob < ApplicationJob
   queue_as :default
 
   def perform(document)
-    # Do something later
-    # 1. Pegar o documento
     xml_file = File.open(ActiveStorage::Blob.service.path_for(document.xml.key))
     xml_doc = Nokogiri::XML(xml_file)
     xml_doc.search("det").each do |det|
-
       taxable = find_product(det)
       next unless taxable
 
@@ -18,19 +15,10 @@ class CreateDocumentTaxesJob < ApplicationJob
         end
       end
     end
-
-    # 2. Atribuir taxas ao recibo
-    # puts "!!!!!!!!!!!!TAXES CREATED!!!!!!!!!!!!"
-    # xml_doc.search('total').each do |total|
-    #   icms_total = total.search('vICMS').first.text.to_f
-    #   product_icms_total = document.products.map { |product| product.taxes.find_by(category: 'ICMS').value }.sum
-    #   puts icms_total === product_icms_total
-    # end
-    # 3. idetificar os produtos e atribuir impostos
   end
 
   def find_product(element)
     name = element.search("prod xProd").first&.text
-    Product.find_by_name(name)
+    Product.find_by_name(name) # || RAISE ERROR
   end
 end
