dae743f90702aed3abb08dd7744353d6a5eee358
Andre Menezes <djdedemenezes@gmail.com>
2024-08-18 20:15:20 -0300

    add devise and onde seed user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_18_145822) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_18_231307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,18 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_18_145822) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["taxable_type", "taxable_id"], name: "index_taxes_on_taxable"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
