1101bbc23e0c6e0565b338717c7c69348dd69fb7
Andre Menezes <djdedemenezes@gmail.com>
2024-08-18 21:14:07 -0300

    add models factories

---
+++ spec/factories/addresses.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :address do
+    logradouro { "MyString" }
+    numero { "MyString" }
+    complemento { "MyString" }
+    bairro { "MyString" }
+    x_municipio { "MyString" }
+    c_municipio { "MyString" }
+    uf { "MyString" }
+    cep { "MyString" }
+    c_pais { "MyString" }
+    x_pais { "MyString" }
+    fone { "MyString" }
+  end
+end
---
+++ spec/factories/companies.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :company do
+    cnpj { "MyString" }
+    x_nome { "MyString" }
+    x_fant { "MyString" }
+    ie { "MyString" }
+    crt { "MyString" }
+    ie_dest { "MyString" }
+    address { nil }
+  end
+end
